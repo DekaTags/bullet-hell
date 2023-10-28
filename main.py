59687ca05bf6abb2bc49d2be958ebd2e932a8cb1
DekaTags <dekatags@gmail.com>
2023-10-28 11:23:12 +0900

    배경 그리기

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import pygame 
 from constants.setting import SCREEN_WIDTH, SCREEN_HEIGHT
+from game.game import Game
 
 pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Hell of Airplane")
 
+game = Game(screen)
+
 running = True
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = false
+            running = False
 
-    
+    game.draw()
     pygame.display.flip()
 
 pygame.quit()
